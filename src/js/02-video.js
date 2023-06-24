d38a11be6c4897f9ea837838dbdc69b1d0772b98
misko_16 <119707421+misko16@users.noreply.github.com>
2023-06-23 18:16:35 -0700

    sdfsdfds

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,12 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// Функція для збереження поточного часу відтворення
 function saveCurrentTime(player) {
   player.getCurrentTime().then((time) => {
     localStorage.setItem('videoplayer-current-time', time);
   });
 }
 
-// Функція для встановлення часу відтворення зі збереженого значення
 function setSavedTime(player) {
   const savedTime = localStorage.getItem('videoplayer-current-time');
 
@@ -17,14 +15,10 @@ function setSavedTime(player) {
   }
 }
 
-// Ініціалізація плеєра
-const player = new Player('vimeo-player'); // змініть 'vimeo-player' на id вашого <iframe>
+const player = new Player('vimeo-player'); 
 
-// Оновлення часу відтворення у локальному сховищі не частіше, ніж раз на секунду
 const saveCurrentTimeThrottled = throttle(() => saveCurrentTime(player), 1000);
 
-// Відстеження події 'timeupdate' для збереження часу відтворення
 player.on('timeupdate', saveCurrentTimeThrottled);
 
-// Встановлення збереженого часу відтворення під час завантаження сторінки
 window.addEventListener('load', () => setSavedTime(player));
